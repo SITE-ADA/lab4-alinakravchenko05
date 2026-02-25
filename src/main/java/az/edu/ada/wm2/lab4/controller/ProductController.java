2e9e87f6da170d0d9705a906fdf966fa8ebd59b0
alinakravchenko05 <151163598+alinakravchenko05@users.noreply.github.com>
2026-02-26 01:40:27 +0400

    Add Model Product

---
+++ src/main/java/az/edu/ada/wm2/lab4/controller/ProductController.java
@@ -0,0 +1,10 @@
+package az.edu.ada.wm2.lab4.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    // methods will be added in the next questions
+}
